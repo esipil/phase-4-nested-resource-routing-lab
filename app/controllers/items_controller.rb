44b5f22db5b49a2ac0bcfea1ed0113f6423361be
Timothy <lemayiant4@gmail.com>
2022-11-25 21:11:54 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
 
   def index
-    items = Item.all
+
+    if params[:user_id] 
+      users = User.find(params[:user_id])
+      items = users.items
+    else 
+      items = Item.all
+    end
+
     render json: items, include: :user
   end
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
 
+  def create 
+    user = User.find(params[:user_id])
+    item = Item.create(item_params)
+    user.items << item
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Dog house not found" }, status: :not_found
+  end
+
+  def item_params 
+    params.permit(:name, :description, :price)
+  end
 end
